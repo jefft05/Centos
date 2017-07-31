bc841be69115b7cb218b8ec9e473705e79031775
Jeff Titus <jdtitus@gmail.com>
2017-07-31 08:37:40 -0400

    Added Python 3

--- sys_setup.sh
+++ sys_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y net-tools links telnet gcc gcc-c++ kernel-devel nmap python-pip wget epel-release git python-devel libselinux-python libxslt-devel libffi-devel openssl-devel bind-utils jq net-snmp net-snmp-utils net-snmp-devel net-snmp-python
+yum install -y net-tools links telnet gcc gcc-c++ kernel-devel nmap python-pip wget epel-release git python-devel libselinux-python python-virtualenv libxslt-devel libffi-devel openssl-devel bind-utils jq net-snmp net-snmp-utils net-snmp-devel net-snmp-python vim-enhanced
 
 git --version
 git config --global user.name "CHANGE NAME"
@@ -10,5 +10,10 @@ git config --list
 
 pip install pip --upgrade
 pip install f5-sdk
-pip install pysnmp1
+pip install pysnmp
+# pip install virtualenv
+# python 3 install
+yum install -y python34 python34-setuptools
+sudo easy_install-3.4 pip
+
 
